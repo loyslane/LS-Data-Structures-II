bf0598fbc7e4f9e9448332b659d3fbce9529dcf3
Lois Truby <loyslane@gmail.com>
2017-07-14 21:03:00 -0400

    completed tree and binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,6 +5,35 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+  insert(value) {
+    if (value > this.value && this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else if (value > this.value) {
+      this.right.insert(value);
+    } else if (value <= this.value && this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+  contains(value) {
+    if (value === this.value) {
+      return true;
+    } else if (value > this.value && !(this.right === null)) {
+      return this.right.contains(value);
+    } else if (value < this.value && !(this.left === null)) {
+      return this.left.contains(value);
+    }
+    return false;
+  }
+  depthFirstForEach(cb) {
+    this.value = cb(this.value);
+    if (!(this.right === null)) {
+      this.right.depthFirstForEach(cb);
+    } else if (!(this.left === null)) {
+      this.left.depthFirstForEach(cb);
+    }
+  }
 }
 
 module.exports = BinarySearchTree;
--- src/tree.js
+++ src/tree.js
@@ -3,6 +3,24 @@ class Tree {
     this.value = value;
     this.children = [];
   }
+  addChild(value) {
+    const tree = new Tree(value);
+    this.children.push(tree);
+  }
+  contains(value) {
+    if (value === this.value) {
+      return true;
+    } else if (this.children.length >= 1) {
+      for (let i = 0; i < this.children.length; i++) {
+        if (value === this.children[i].value) {
+          return true;
+        } else if (this.children[i].contains(value)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
 
 module.exports = Tree;
